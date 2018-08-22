cac4b63d7184deb9513142e9d64938aa685f4042
cyan.luo <cyan.luo@corp.to8to.com>
2018-08-22 17:11:58 +0800

    updata project

---
+++ src/main/java/org/keywordsFramework/configuration/Constans.java
@@ -0,0 +1,30 @@
+package org.keywordsFramework.configuration;
+
+public class Constans {
+    //测试用例路径
+    public static final String Path_ExcelFile = "./src/main/java/org/keywordsFramework/data/关键字驱动测试用例.xlsx";
+
+    //页面元素路径
+    public static final String Path_ConfigurationFile = "./ObjectMap.properties";
+
+    //测试数据sheet中的列号常量设定
+    public static final int Col_TestCaseID = 0;
+
+    public static final int Col_KeyWordAction = 3;
+
+    public static final int Col_LocatorExpression = 4;
+
+    public static final int Col_ActionValue = 5;
+
+    public static final int Col_TestStepTestRuselt = 6;
+
+    public static final int Col_RunFlag = 2;
+
+    public static final int Col_TestRuiteTestRuselt = 3;
+
+    public static final String Sheet_TestSteps = "Sheet1";
+
+    public static final String Sheet_TestSuite = "测试用例集合";
+
+
+}
